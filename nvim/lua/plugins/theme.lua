94e4fb251e48d0bb24f12a397e3f40fe912318d8
afairi-asatar <131270135+afairi-asatar@users.noreply.github.com>
2024-10-29 22:36:59 +0100

    new themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,6 +1,19 @@
 return {
-    {
-    "aktersnurra/no-clown-fiesta.nvim",
-    transparent = true, -- Enable this to disable the bg color
-  }
+  { "aktersnurra/no-clown-fiesta.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "sainnhe/everforest" },
+  { "bluz71/vim-nightfly-colors" },
+  { "NLKNguyen/papercolor-theme" },
+  { "romainl/Apprentice" },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
